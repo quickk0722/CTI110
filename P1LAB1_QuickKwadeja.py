a8fd7bdfafc09bc2b88f35b20537089893fb9196
quickk0722 <quickk0722@student.faytechcc.edu>
2024-09-22 10:46:00 -0400

    Add files via upload
    First Submission

---
+++ P1LAB1_QuickKwadeja.py
@@ -0,0 +1,7 @@
+#Kwadeja Quick
+#09-20-2024
+#CTI-P1LAB1
+#Python program using IDLE showing how to get input from the user and displays output.
+first_name = input("Enter your first name:  ") 
+last_name = input("Enter your last name:  ") 
+print('Hello,', first_name, last_name + '! Welcome to CTI-110')
