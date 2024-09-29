eb833a99a190c0f762a993c153192ad6d6ae3c6a
quickk0722 <quickk0722@student.faytechcc.edu>
2024-09-29 08:50:02 -0400

    Add files via upload
    Python program using mathematical expressions

---
+++ P1HW1_QuickKwadeja.py
@@ -0,0 +1,22 @@
+#Kwadeja Quick
+#September 28, 2024
+#P1HW1
+#Python program using mathematical expressions
+#Calculating exponents
+print("----Calculating exponents----")
+print("\n")
+base = int(input("Enter an integer as the base value: "))
+exponent = int(input("Enter an integer as the exponent: "))
+print("\n")
+result_exponent = base ** exponent
+print(f"{base} raised to the power of {exponent} is {result_exponent} !!")
+print("\n\n")
+#Addition and Subtraction
+print("----Addition and Subtraction----")
+print("\n\n")
+starting_integer = int(input("Enter a starting integer: "))
+add_integer = int(input("Enter an integer to add: "))
+subtract_integer = int(input("Enter an integer to subtract: "))
+print("\n\n")
+result_add_subtract = starting_integer + add_integer - subtract_integer
+print(f"{starting_integer} + {add_integer} - {subtract_integer} is equal to {result_add_subtract}.")
